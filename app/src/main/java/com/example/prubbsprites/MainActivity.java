6cd08d1ed7f33a82c2fd40f1e74c572eb4a5b81c
Juan Carlos <Juancarmgc2017@gmail.com>
2024-02-19 12:05:12 +0100

    commit contenido

--- app/src/main/java/com/example/prubbsprites/MainActivity.java
+++ app/src/main/java/com/example/prubbsprites/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.prubbsprites;
 
 import android.graphics.drawable.AnimatedImageDrawable;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,10 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer media;
+    MediaPlayer media_sd;
     private ImageView imageView;
     private ImageView enemigoImageView;
     private AnimatedImageDrawable animatedImageDrawable;
-    private int defaultGifResource = R.drawable.ryupose;
+    private int defaultGifResource = R.drawable.asra;
     private int newGifResource;
     private int enemigoPosition = 0;
     private int distanciaDanio = 500;
@@ -25,14 +28,26 @@ public class MainActivity extends AppCompatActivity {
     private int anchoOriginal = 271;
     private int alturaOriginal = 126;
     private int limitePantalla = -1500;
+    private int nuevoAnchoMovimiento = 450; // Nuevo ancho del GIF durante el movimiento
+    private int nuevaAlturaMovimiento = 350; // Nueva altura del GIF durante el movimiento
+    private boolean isMoving = false; // Indica si el personaje está en movimiento
+    private static final int MOVEMENT_DURATION = 1000; // Duración del movimiento en milisegundos
+    private float currentTranslationX = 0; // Posición actual de la imagen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(media==null){
+            media = MediaPlayer.create(this,R.raw.sound);
+        }
+
+        if ((!media.isPlaying())){
+            media.start();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
-        defaultGifResource = R.drawable.ryupose;
+        defaultGifResource = R.drawable.asra;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             animatedImageDrawable = (AnimatedImageDrawable) imageView.getDrawable();
         }
@@ -64,9 +79,12 @@ public class MainActivity extends AppCompatActivity {
         moveRightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float currentTranslationX = imageView.getTranslationX();
-                float translationXChange = 20;
-                imageView.setTranslationX(currentTranslationX + translationXChange);
+                // Cambiar el GIF del personaje al mover a la derecha
+                loadNewGif(R.drawable.asrandar, nuevoAnchoMovimiento, nuevaAlturaMovimiento);
+                isMoving = true;
+
+                currentTranslationX += 20; // Incrementar la posición actual
+                imageView.setTranslationX(currentTranslationX);
             }
         });
 
@@ -74,9 +92,12 @@ public class MainActivity extends AppCompatActivity {
         moveLeftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float currentTranslationX = imageView.getTranslationX();
-                float translationXChange = -20;
-                imageView.setTranslationX(currentTranslationX + translationXChange);
+                // Cambiar el GIF del personaje al mover a la izquierda
+                loadNewGif(R.drawable.asrandar, nuevoAnchoMovimiento, nuevaAlturaMovimiento);
+                isMoving = true;
+
+                currentTranslationX -= 30; // Decrementar la posición actual
+                imageView.setTranslationX(currentTranslationX);
             }
         });
 
@@ -105,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                enemigoPosition -= 20;
+                enemigoPosition -= 30;
                 enemigoImageView.setTranslationX(enemigoPosition);
 
                 if (enemigoPosition <= limitePantalla - enemigoImageView.getWidth()) {
@@ -132,8 +153,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadNewGif() {
-        newGifResource = R.drawable.samurr;
-        defaultGifResource = R.drawable.samurai;
+        newGifResource = R.drawable.asrattak;
+        defaultGifResource = R.drawable.asra;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             if (animatedImageDrawable != null && animatedImageDrawable.isRunning()) {
@@ -168,8 +189,61 @@ public class MainActivity extends AppCompatActivity {
                     params.width = anchoOriginal;
                     params.height = alturaOriginal;
                     imageView.setLayoutParams(params);
+
+                    // Restablecer la posición original después de un tiempo
+                    resetImageViewPosition();
                 }
             }, 900);
         }
+    }
+
+    private void loadNewGif(int resource, int newWidth, int newHeight) {
+        newGifResource = resource;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            if (animatedImageDrawable != null && animatedImageDrawable.isRunning()) {
+                animatedImageDrawable.stop();
+            }
+        }
+
+        ViewGroup.LayoutParams params = imageView.getLayoutParams();
+        params.width = newWidth; // Usar el nuevo ancho proporcionado
+        params.height = newHeight; // Usar la nueva altura proporcionada
+        imageView.setLayoutParams(params);
+
+        imageView.setImageResource(newGifResource);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            animatedImageDrawable = (AnimatedImageDrawable) imageView.getDrawable();
+            animatedImageDrawable.start();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    imageView.setImageResource(defaultGifResource);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        animatedImageDrawable = (AnimatedImageDrawable) imageView.getDrawable();
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        if (animatedImageDrawable != null && animatedImageDrawable.isRunning()) {
+                            animatedImageDrawable.stop();
+                        }
+                    }
+
+                    ViewGroup.LayoutParams params = imageView.getLayoutParams();
+                    params.width = anchoOriginal;
+                    params.height = alturaOriginal;
+                    imageView.setLayoutParams(params);
+
+                    // Restablecer la posición original después de un tiempo
+                    resetImageViewPosition();
+                }
+            }, 500); // Cambiar el tiempo de espera a 500 milisegundos (0.5 segundos)
+        }
+    }
+
+    private void resetImageViewPosition() {
+        // Restablecer la posición original de la vista imageView
+        imageView.setTranslationX(currentTranslationX); // Mantener la posición actual
+        isMoving = false;
     }
 }
